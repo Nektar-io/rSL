8fe0ef490c49c2b8d60b22345805afc2a2cd77c9
Love Hansson <love.hansson@gmail.com>
2014-01-06 19:00:23 +0100

    Fix: When no travel time is returned, return 0 instead of an error. Also raise a warning.

--- R/traveltime.R
+++ R/traveltime.R
@@ -4,9 +4,16 @@
 #' 
 #' @param coords A vector of WGS coordinates on [northing, easting] format for the original point of travel
 #' @param destinations The destination station IDs we want to calculate travel times to. Default is 9001, which is T-Centralen. Other examples include 9192 (Slussen) and 9117 (Odenplan).
+#' @param time A character string on the "HH:MM" format.
+#' @param date A character string on the "DD.MM.YYYY" format.
 #' @export
 
-travelTimeFromPos <- function(WGS, destinations=c(9001)) {
+travelTimeFromPos <- function(
+	WGS,
+	destinations = c(9001),
+	Time = "12:00",
+	Date = strftime(today(), "%d.%m.%Y")
+) {
 	# Get point of origin in an API friendly format
 	SID <- paste0("@Y=",format(WGS*10^6, digits=8)[1],"@X=",format(WGS*10^6, digits=8)[2])
 	
@@ -22,11 +29,27 @@ travelTimeFromPos <- function(WGS, destinations=c(9001)) {
 				key = .reseplanerareKey,
 				SID = SID,
 				Z = destinations[dest],
-				Time = "12:00"
+				Time = Time,
+				Date = Date
 			))
 		
 		x <- GET(url)
 		xdata <- xmlToList(xmlParse(x), simplify = TRUE)
+		
+		# Error handling: If the first element of xdata is not a list, an error
+		# has been returned instead of travel data. However, we don't want the
+		# function to crash so we assume the reason for this is that this is because
+		# SID and Z are too close. Hence we return "0" instead.
+		if (is.character(xdata[[1]])) {
+			warning(
+				"In rSL::travelTimeFromPos():\n",
+				"An error was returned from the SL travel time API.\n",
+				"Since the API does this when the address is too close to the destination,\n",
+				"we simply assume that this means that the travel time is zero."
+			)
+			sumtime[[dest]] <- 0
+			next()
+		}
 		
 		sumtime[[dest]] <- 0
 		for (i in 3:7) {
