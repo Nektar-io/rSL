a380e5269fe4c0d52a8ee8216b5ea7e9cc660b09
LCHansson <love.hansson@gmail.com>
2014-07-07 20:55:32 +0200

    Add API keys for alternative API (probably not needed)

--- R/0-constants.R
+++ R/0-constants.R
@@ -3,3 +3,6 @@
 .reseplanerareKey <- "dk5ZlsqLXiEdyHYVfTR2y56GR63a6Ejs"
 .reseplanerareUrl <- "https://api.trafiklab.se"
 .reseplanerarePath <- "sl/reseplanerare.xml"
+
+.resrobotKey <- "5x19EcaZ8sryuPBc6wJvQUuz1dJgWEK9"
+.resrobotUrl <- "https://api.trafiklab.se/samtrafiken/resrobotsuper/Search"
